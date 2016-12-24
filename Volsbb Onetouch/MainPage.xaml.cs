a1dbdcfdedbce433fbdb6f23f7927daac1b5b603
bitloks <bitloks@gmail.com>
2016-12-24 21:32:55 +0530

    app basic backend ready

--- Volsbb Onetouch/MainPage.xaml.cs
+++ Volsbb Onetouch/MainPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Uwp;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Networking.Connectivity;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +25,35 @@ namespace Volsbb_Onetouch
     /// </summary>
     public sealed partial class MainPage : Page
     {
+      
+
+        
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async  void Button_Click(object sender, RoutedEventArgs e)
+        {
+          
+
+            var access= await BackgroundExecutionManager.RequestAccessAsync();
+           //   var wlanConnectionProfileDetails = connectionProfile.wlanConnectionProfileDetails;
+
+           
+        //    var SSID = WlanConnectionProfileDetailss.GetConnectedSsid();
+
+            BackgroundTaskHelper.Register("SampleBackgroundTask", "Tasks.SampleBackgroundTask", new SystemTrigger(SystemTriggerType.NetworkStateChange, false), false, true, null);
+            BackgroundTaskHelper.Register("ToastBackgroundTask", "Tasks.ToastBackgroundTask", new ToastNotificationActionTrigger(), false,false, null);
+
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            BackgroundTaskHelper.Unregister("SampleBackgroundTask");
+            BackgroundTaskHelper.Unregister("ToastBackgroundTask");
         }
     }
 }
