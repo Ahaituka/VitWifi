17ff7770a69169ebd02194dd6b6c0cc83d357676
bitloks <bitloks@gmail.com>
2016-12-30 21:34:23 +0530

    Added  Network Helper Methods

---
+++ Network/Pronto.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Notifications;
+
+namespace Network
+{
+    public class Pronto
+        {
+
+
+        public static async Task<string> Login()
+        {
+            return null;
+
+        }
+
+        public static async Task<string> Logout()
+        {
+            return null;
+
+        }
+
+
+        public static async Task<string> DataUsage()
+        {
+            return null;
+
+        }
+
+        public static  void PopToast(string msg)
+        {
+            // Generate the toast notification content and pop the toast
+            ToastContent content = new ToastContent()
+            {
+                Launch = "action=viewEvent&eventId=1983",
+                Scenario = ToastScenario.Reminder,
+
+                Visual = new ToastVisual()
+                {
+                    BindingGeneric = new ToastBindingGeneric()
+                    {
+                        Children =
+                {
+                    new AdaptiveText()
+                    {
+                        Text = msg
+                    },
+
+                }
+                    }
+                },
+
+
+            };
+            ToastNotificationManager.CreateToastNotifier().Show(new ToastNotification(content.GetXml()));
+        }
+
+
+
+    }
+}
